197bb0787b272d0b2e37cbc0d4d51838685a8b1f
Leeuc <92780253+Leewuc@users.noreply.github.com>
2023-10-27 11:05:55 +0900

    Add files via upload

---
+++ Handling Favorite Button/Model/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  Landmakes_1
+//
+//  Created by 이우창 on 10/26/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable,Codable,Identifiable {
+    var id : Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    var isFavorite: Bool
+    
+    private var imageName: String 
+    var image :Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinates: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude, 
+            longitude: coordinates.longtitude)
+    }
+    
+    struct Coordinates : Hashable, Codable {
+        var latitude: Double
+        var longtitude: Double
+    }
+    
+}
