7e6fbcb286cc558e4de4db7f95d0b2cd578c28e0
Leeuc <92780253+Leewuc@users.noreply.github.com>
2023-10-27 10:36:20 +0900

    Add files via upload

---
+++ Bulding lists and navigation/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmakes_1
+//
+//  Created by 이우창 on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Bulding lists and navigation/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmakes_1
+//
+//  Created by 이우창 on 10/27/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
